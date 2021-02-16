2ca982c638ecd011ba98247c3f68c1fc5c7809c1
Kedong Shao <kshao5@illinois.edu>
2021-02-15 18:12:59 -0800

    initial commit to github

---
+++ app/src/main/java/com/example/knowledgeplus/Register.java
@@ -0,0 +1,84 @@
+package com.example.knowledgeplus;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
+
+public class Register extends AppCompatActivity {
+    EditText fullName, email, password;
+    Button registerButton;
+    FirebaseAuth fAuth;
+    TextView loginText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        fullName = findViewById(R.id.fullName);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        registerButton = findViewById(R.id.registerButton);
+        loginText = findViewById(R.id.loginText);
+
+
+        fAuth = FirebaseAuth.getInstance();
+
+        // user has already loggedin
+        if(fAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String passwordString = password.getText().toString().trim();
+                String emailString = email.getText().toString().trim();
+                if (TextUtils.isEmpty(passwordString)) {
+                    password.setError("Password is Required");
+                    return;
+                }
+
+                // register the user in firebase
+                fAuth.createUserWithEmailAndPassword(emailString, passwordString).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(Register.this, "User Created.", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(Register.this, "Error !" + task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        loginText.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+            }
+        });
+    }
+
+
+}
