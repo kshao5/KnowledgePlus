79cca84d4f7ff0e70ab4d55e2d697f48ecb1c6af
taipeng <lyutiepeng@gmail.com>
2021-03-08 15:08:18 -0800

    fix update nComments bug

--- app/src/main/java/com/example/knowledgeplus/articleDetail.java
+++ app/src/main/java/com/example/knowledgeplus/articleDetail.java
@@ -72,7 +72,7 @@ public class articleDetail extends AppCompatActivity {
                     commentString += "\n\n" + comment.getUsername() +  ", " + comment.getDate() + ":\n" + comment.getText();
                 }
                 tvComment.setText(commentString);
-                String nComments = ""+snapshot.getChildrenCount();
+                int nComments = (int)snapshot.getChildrenCount();
                 tvNComments.setText(nComments);
                 FirebaseDatabase.getInstance().getReference("article").child(articleCard.getId()).child("nComments").setValue(nComments);
             }
